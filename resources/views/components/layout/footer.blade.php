5fc6196a96925e4e9ed5a2d2c10e73af14f38be1
HariyatiN <hariyatiningsih124@gmail.com>
2023-12-06 20:56:08 +0700

    memperbaiki footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,7 +1,14 @@
-<footer class="footer " style="position: fixed ; bottom:0;">
-    <div class="container">
-      <div class="text-center">
-        Copyright ©2023 BANK DARAH
-      </div>
+<footer class="container-fluid  " >
+    <div class="bg-secondary p-4 rounded" >
+        <div class="row">
+            <div class="col-12 col-sm-6 text-center text-sm-start">
+                &copy; <a href="#">DBLOOD</a>, All Right Reserved. 
+            </div>
+            <div class="col-12 col-sm-6 text-center text-sm-end">
+                <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+            </div>
+        </div>
     </div>
-  </footer>
+</footer>
