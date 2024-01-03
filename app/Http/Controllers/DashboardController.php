c4961bbe3eed54c44ce715a525b5aecafcb21c93
HariyatiN <hariyatiningsih124@gmail.com>
2024-01-03 23:41:26 +0700

    menambahkan total produk darah di dashboard dan memperbaiki notif

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,13 +6,35 @@ use Illuminate\Http\Request;
 use App\Models\Pendonor;
 use App\Models\Admin;
 use App\Models\Info;
+use App\Models\Darah;
+use App\Models\Produk;
 class DashboardController extends Controller
 {
-    function index(){
+    function index(Produk $produk){
 
         $data['totalAdmins'] = Admin::count();
         $data['totalPendonor'] = Pendonor::count();
         $data['totalInfo'] = Info::count();
+        $data['totalDarah'] = Darah::count();
+        $darah = Darah::with(['produk'])->get();
+
+      // Calculate total stock for each product
+      $totalStock = [];
+      foreach ($darah as $item) {
+          $productId = $item->produk->id;
+
+          if (!isset($totalStock[$productId])) {
+              $totalStock[$productId] = [
+                  'product' => $item->produk,
+                  'total' => 0,
+              ];
+          }
+
+          $totalStock[$productId]['total'] += $item->A + $item->B + $item->O + $item->AB;
+      }
+
+      $data['totalStock'] = $totalStock;
+
         return view("welcome", $data);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,16 +24,16 @@
         </div>
         <div class="col-sm-6 col-xl-3">
             <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-chart-area fa-3x text-primary"></i>
+                <i class="fa fa-tint fa-3x text-primary"></i>
                 <div class="ms-3">
-                    <p class="mb-2">Total Darah</p>
-                    <h6 class="mb-0 text-dark">$1234</h6>
+                    <p class="mb-2">Produk Darah</p>
+                    <h6 class="mb-0 text-dark">{{ $totalDarah }}</h6>
                 </div>
             </div>
         </div>
         <div class="col-sm-6 col-xl-3">
             <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
-                <i class="fa fa-chart-pie fa-3x text-primary"></i>
+                <i class="fa fa-info-circle fa-3x text-primary"></i>
                 <div class="ms-3">
                     <p class="mb-2">Info</p> 
                     <h6 class="mb-0 text-dark">{{ $totalInfo }}</h6>
@@ -42,4 +42,23 @@
         </div>
     </div>
 </div>
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        @foreach ($totalStock as $item)
+            <div class="col-sm-12 col-xl-4">
+                <div class="bg-secondary text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h3 class="mb-0 text-dark">{{ $item['product']->produk }}</h3>
+                        <a href="{{ url('/admin/darah') }}">Details</a>
+                    </div>
+                    
+                    <p>Total Stok Darah </p>
+                    <h6 class="mb-0 text-dark">{{ $item['total'] }}</h6>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
 </x-app>
